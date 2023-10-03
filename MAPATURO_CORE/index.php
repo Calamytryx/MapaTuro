3bedbac1e660b85bcae5933446a6eadbef25b83b
Renzo <91522080+Reganexus@users.noreply.github.com>
2023-10-04 01:11:08 +0800

    MAPATURO - HUGE PATCH
    - Applied CSS
    - Functions (dropdown)
    - UI almost complete

--- MAPATURO_CORE/index.php
+++ MAPATURO_CORE/index.php
@@ -4,16 +4,41 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>MAPATURO</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <?php include("links.php"); ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/nav.css">
+    <link rel="stylesheet" href="CSS/svg-map.css">
+    <link rel="stylesheet" href="CSS/remote-buttons.css">
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;600&display=swap');
+    </style>
 </head>
 
 <body>
-    <div class="container-fluid" id="main-container">
-        <?php include("nav.php"); ?>
+    <div class="main-container">
+        <div class="container-fluid" id="svg-map">
+            <div class="map-wrapper">
+                <?php include("svg-map.php"); ?>
+            </div>
+        </div>
 
-        <?php include("svg-map.php"); ?>
+        <div class="remote-buttons text-center" id="remote-buttons">
+            <div class="remote-button-wrapper">
+                <?php include("remote-buttons.php"); ?>
+            </div>
+        </div>
+
+        <div class="navigation-bar container-fluid ">
+            <?php include("nav.php"); ?>
+        </div>
     </div>
 
 
@@ -22,9 +47,12 @@
 
 
 
-
-
-    <?php include("scripts.php"); ?>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
--- MAPATURO_CORE/nav.php
+++ MAPATURO_CORE/nav.php
@@ -1,161 +1,44 @@
-<header>
-    <nav class="navbar navbar-expand-lg px-lg-5 p-2">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="Pictures/nav_logo_color.png" alt="MAPATURO Logo"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item px-2">
-                        <a class="nav-link" href="#">About Us</a>
-                    </li>
-                    <li class="nav-item px-2 dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Language: <span id="selectedLanguage">English</span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#" onclick="changeLanguage('English')">English</a></li>
-                            <li><a class="dropdown-item" href="#" onclick="changeLanguage('Filipino')">Filipino</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item px-2">
-                        <a class="nav-link" onclick="toggleAccountsModal()">
-                            <i class="fa-regular fa-circle-user"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header>
-
-
-<section class="accounts-hidden" id="accounts-modal">
-    <div class="container-fluid" id="accounts">
-        <div class="px-4 py-5 px-md-5 text-center text-lg-start" style="background-color: #F5F5F5">
-            <div class="close-btn text-end m-2" id="close-button-accounts">
-                <span><i class="fa-solid fa-xmark"></i></span>
-            </div>
-            <div class="row gx-lg-5 align-items-center">
-                <div class="col-lg-6 mb-5 mb-lg-0">
-                    <h1 class="my-5 display-3 fw-bold ls-tight">
-                        Sign Up at<br />
-                        <span class="text-primary">MAPATURO</span>
-                    </h1>
-                    <p style="color: hsl(217, 10%, 50.8%)">
-                        Create an account today to learn more about the geography and wonders of the Philippines.
-                        Collect points and compete with other users with different game modes and game plays.
-                    </p>
-                </div>
-
-                <div class="col-lg-6 mb-5 mb-lg-0">
-                    <div class="card">
-                        <div class="card-body py-5 px-md-5">
-                            <form>
-                                <!-- 2 column grid layout with text inputs for the first and last names -->
-                                <div class="row">
-                                    <div class="col-md-6 mb-4">
-                                        <div class="form-outline">
-                                            <input type="text" id="form3Example1" class="form-control" />
-                                            <label class="form-label" for="form3Example1">First name</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6 mb-4">
-                                        <div class="form-outline">
-                                            <input type="text" id="form3Example2" class="form-control" />
-                                            <label class="form-label" for="form3Example2">Last name</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <!-- Email input -->
-                                <div class="form-outline mb-4">
-                                    <input type="email" id="form3Example3" class="form-control" />
-                                    <label class="form-label" for="form3Example3">Email address</label>
-                                </div>
-
-                                <!-- Password input -->
-                                <div class="form-outline mb-4">
-                                    <input type="password" id="form3Example4" class="form-control" />
-                                    <label class="form-label" for="form3Example4">Password</label>
-                                </div>
-
-                                <!-- Checkbox -->
-                                <div class="form-check d-flex justify-content-center mb-4">
-                                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33"
-                                        checked />
-                                    <label class="form-check-label" for="form2Example33">
-                                        Remember Me
-                                    </label>
-                                </div>
-
-                                <!-- Submit button -->
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-primary btn-block mb-4">
-                                        Sign up
-                                    </button>
-                                </div>
-
-                                <!-- Register buttons -->
-                                <div class="text-center">
-                                    <p>or sign up with:</p>
-                                    <button type="button" class="btn btn-link btn-floating mx-1">
-                                        <i class="fab fa-facebook-f"></i>
-                                    </button>
-
-                                    <button type="button" class="btn btn-link btn-floating mx-1">
-                                        <i class="fab fa-google"></i>
-                                    </button>
-
-                                    <button type="button" class="btn btn-link btn-floating mx-1">
-                                        <i class="fab fa-twitter"></i>
-                                    </button>
-
-                                    <button type="button" class="btn btn-link btn-floating mx-1">
-                                        <i class="fab fa-github"></i>
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<nav class="navbar navbar-expand-lg px-lg-3 px-1">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="Pictures/nav_logo_color.png" alt="Logo"></a>
+        <ul class="navbar-nav">
+            <li class="nav-item mx-2 px-2">
+                <a class="nav-link p-2" aria-disabled="true">ABOUT US</a>
+            </li>
+        </ul>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item mx-2 px-2 dropdown">
+                    <a class="nav-link p-2 dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        LANGUAGE <span id="selected-language">EN</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#" onclick="changeLanguage('EN')">ENGLISH</a></li>
+                        <li><a class="dropdown-item" href="#" onclick="changeLanguage('FIL')">FILIPINO</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item mx-2 px-2">
+                    <a class="nav-link p-2" aria-disabled="true">SIGN IN</a>
+                </li>
+                <li class="nav-item mx-2 px-2">
+                    <a class="nav-link p-2" aria-disabled="true">LOG IN</a>
+                </li>
+            </ul>
         </div>
     </div>
-</section>
-
-
-
-
+</nav>
 
 <script>
-    // Change Language Function
-    function changeLanguage(language) {
-        document.getElementById("selectedLanguage").textContent = language;
+    function changeLanguage(languageCode) {
+        var languageText = (languageCode === 'EN') ? 'EN' : 'FIL';
+        document.getElementById('selected-language').textContent = languageText;
     }
 
-    // Hide and Display Account Container
-    function toggleAccountsModal() {
-        var accountsModal = document.getElementById("accounts-modal");
-        if (accountsModal.classList.contains("accounts-hidden")) {
-            accountsModal.classList.remove("accounts-hidden");
-            accountsModal.classList.add("accounts-visible");
-        } else {
-            accountsModal.classList.remove("accounts-visible");
-            accountsModal.classList.add("accounts-hidden");
-        }
-    }
 
-    // Close Button Function
-    var closeButton = document.getElementById("close-button-accounts");
-    closeButton.addEventListener("click", hideAccountsModal);
 
-    function hideAccountsModal() {
-        var accountsModal = document.getElementById("accounts-modal");
-        accountsModal.classList.remove("accounts-visible");
-        accountsModal.classList.add("accounts-hidden");
-    }
 </script>
